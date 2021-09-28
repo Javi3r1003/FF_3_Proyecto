dcf14b9799deb6c2c0e749bbac3cd17f037627d1
Javier Alecio <63657473+Javi3r1003@users.noreply.github.com>
2021-09-28 02:33:15 -0600

    cuadros y movimiento de las partículas
    Sexoooooooooooooooo

--- Primer.py
+++ Primer.py
@@ -2,11 +2,12 @@ import tkinter as tk
 from tkinter import *
 import tkinter.font as tkFont
 
+D = 2
+mole = []
 
-#JaviGayyyyy y Pablitoooooooooooooooooooo
 
 class Moleculas:
-	def __init__(self, canvas, x, y, w, h, Yo, mass = 0, Xo = -275, color = 'blue'):
+	def __init__(self, canvas, x, y, w, h, Yo, mass = 1, q = -1, Xo = -275, color = 'blue'):
 		self.canvas = canvas
 
 
@@ -15,8 +16,29 @@ class Moleculas:
 		self.vel = [0,0]
 		self.acc = [0,0]	
 		self.body = self.canvas.create_oval(x-w/2, y-h/2, x+w/2, x+h/2, fill = color)
+		self.mass = mass
+		self.q = q
 
-		self.canvas.move(self.body, -275, 275)
+		self.canvas.move(self.body, -275, Yo)
+
+	def Force(self, Px, Py):
+		global D
+		self.acc[0] = (self.q*Px)/(self.mass*D)
+		self.acc[1] = (self.q*Py)/(self.mass*D)
+
+	def update(self, t, P1, P2):
+		self.x, self.y, *c = self.canvas.coords(self.body)
+
+		self.Force(P1, P2)
+
+		self.vel[0] = self.vel[0] + self.acc[0]*t
+		self.vel[1] = self.vel[1] + self.acc[1]*t
+
+		self.canvas.move(self.body, -self.vel[0], self.vel[1])
+
+
+
+
 
 		
 
@@ -39,17 +61,67 @@ class aplic():
         self.Can = Canvas(self.root, bg = 'white', height = h, width = w)
         self.Can.pack()
 
-        p1 = self.Can.create_rectangle(50, 0, 650, 5, fill = 'black')
-        self.Can.move(p1, 15, 675)
 
-        p2 = self.Can.create_rectangle(50, 0, 650, 5, fill = 'black')
-        self.Can.move(p2, 15, 575)
+#Vista Superior
+        p1 = self.Can.create_rectangle(0, 0, 250, 1, fill ="black")
+        self.Can.move(p1, 50, 651)
+
+        p2 = self.Can.create_rectangle(0, 0, 250, 1, fill ="black")
+        self.Can.move(p2, 50, 550)
+
+        p3 = self.Can.create_rectangle(0, 0, 550, 1, fill ="black")
+        self.Can.move(p3, 50, 600)
+
+        p4 = self.Can.create_line(250, 1, 550, 50, width = 2)
+        self.Can.move(p4, 50, 650)
+
+        p5 = self.Can.create_line(250, 1, 550, -50, width = 2)
+        self.Can.move(p5, 50, 550)
+
+        p6 = self.Can.create_line(550, -50, 550, 151)
+        self.Can.move(p6, 50, 550)
+
+        p8 = self.Can.create_line(250, 0, 250, 101)
+        self.Can.move(p8, -200, 550)
 
 
-        m = Moleculas(self.Can, 350, 350, 10, 10, 275)
+        Ms = Moleculas(self.Can, 350, 350, 10, 10, 251)
+        mole.append(Ms)
+
+
+#Vista Lateral
+        p1 = self.Can.create_rectangle(0, 0, 250, 1, fill ="black")
+        self.Can.move(p1, 50, 251)
+
+        p2 = self.Can.create_rectangle(0, 0, 250, 1, fill ="black")
+        self.Can.move(p2, 50, 150)
+
+        p3 = self.Can.create_rectangle(0, 0, 550, 1, fill ="black")
+        self.Can.move(p3, 50, 200)
+
+        p4 = self.Can.create_line(250, 1, 550, 50, width = 2)
+        self.Can.move(p4, 50, 250)
+
+        p5 = self.Can.create_line(250, 1, 550, -50, width = 2)
+        self.Can.move(p5, 50, 150)
+
+        p6 = self.Can.create_line(550, -50, 550, 151)
+        self.Can.move(p6, 50, 150)
+
+        p8 = self.Can.create_line(250, 0, 250, 101)
+        self.Can.move(p8, -200, 150)
+
+
+        Ml = Moleculas(self.Can, 350, 350, 10, 10, -149)
+        mole.append(Ml)
 
 
 
+        while True:
+        	self.root.update()
+        	print("esta")
+        	for m in mole:
+        		m.update(.0001, 50, 0)
 
         self.master.mainloop()
 
