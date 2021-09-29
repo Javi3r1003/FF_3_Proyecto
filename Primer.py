f68ae0e5765d9326278fe8ecb4a7d4df9cc9efda
Javier Alecio <63657473+Javi3r1003@users.noreply.github.com>
2021-09-29 03:48:16 -0600

    Prueba de gráfica

--- Primer.py
+++ Primer.py
@@ -1,11 +1,19 @@
 import tkinter as tk
 from tkinter import *
 import tkinter.font as tkFont
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 D = 2
 mole = []
 X = []
 Y = []
+
+fig = plt.Figure()
 
 
 class Moleculas:
@@ -34,11 +42,15 @@ class Moleculas:
     def update(self, t, P1, P2):
         self.x, self.y, *c = self.canvas.coords(self.body)
 
-        if self.x < 595:
+        if self.x < 382:
+
+            
             self.Force(P1, P2)
             self.vel[0] = self.vel[0] + self.acc[0]*t
             self.vel[1] = self.vel[1] + self.acc[1]*t
             self.canvas.move(self.body, -self.vel[0], self.vel[1])
+            
+
         else:
             if self.tipo == "S":
                 X.append(self.y)
@@ -72,28 +84,31 @@ class aplic():
         self.Can = Canvas(self.root, bg = 'white', height = h, width = w)
         self.Can.pack()
 
+        self.graph = Canvas(self.Can, bg = 'black', height = 750, width = 635)
+        self.graph.place(x = 650, y = 40)
+
 
 #Vista Superior
-        p1 = self.Can.create_rectangle(0, 0, 250, 1, fill ="black")
-        self.Can.move(p1, 50, 651)
+        p1 = self.Can.create_rectangle(0, 0, 120, 1, fill ="black")
+        self.Can.move(p1, 50, 650)
 
-        p2 = self.Can.create_rectangle(0, 0, 250, 1, fill ="black")
-        self.Can.move(p2, 50, 550)
+        p2 = self.Can.create_rectangle(0, 0, 120, 1, fill ="black")
+        self.Can.move(p2, 50, 611)
 
-        p3 = self.Can.create_rectangle(0, 0, 550, 1, fill ="black")
-        self.Can.move(p3, 50, 600)
+        p3 = self.Can.create_rectangle(0, 0, 341, 1, fill ="black")
+        self.Can.move(p3, 50, 630)
 
-        p4 = self.Can.create_line(250, 1, 550, 50, width = 2)
+        p4 = self.Can.create_line(120, 0, 341, 129, width = 2)
         self.Can.move(p4, 50, 650)
 
-        p5 = self.Can.create_line(250, 1, 550, -50, width = 2)
-        self.Can.move(p5, 50, 550)
+        p5 = self.Can.create_line(120, 0, 341, -143, width = 2)
+        self.Can.move(p5, 50, 611)
 
-        p6 = self.Can.create_line(550, -50, 550, 151)
-        self.Can.move(p6, 50, 550)
+        p6 = self.Can.create_line(341, -156, 341, 156)
+        self.Can.move(p6, 50, 624)
 
-        p8 = self.Can.create_line(250, 0, 250, 101)
-        self.Can.move(p8, -200, 550)
+        p8 = self.Can.create_line(250, 31, 250, 70)
+        self.Can.move(p8, -200, 580)
 
 
         
@@ -120,27 +135,10 @@ class aplic():
 
         p8 = self.Can.create_line(250, 31, 250, 70)
         self.Can.move(p8, -200, 150)
-        
-        
-#Cuadritooo
-        p1 = self.Can.create_rectangle(0, 0, 312, 1, fill ="black")
-        self.Can.move(p1, 800, 100)
-        
-        p2 = self.Can.create_rectangle(0, 0, 312, 1, fill ="black")
-        self.Can.move(p2, 800, 412)
-        
-        p3 = self.Can.create_rectangle(0, 0, 1, 312, fill ="black")
-        self.Can.move(p3, 800, 100)
-        
-        p3 = self.Can.create_rectangle(0, 0, 1, 312, fill ="black")
-        self.Can.move(p3, 1112, 100)
-        
-        
-        
 
         #Frame para los botones
-        W = Frame(self.Can, height = 300, width = 850, bg = "#F0B27A")
-        W.place(x = 665, y = 520)
+        W = Frame(self.Can, height = 300, width = 1050, bg = "#F0B27A")
+        W.place(x = 465 , y = 520)
 
         fontt = tkFont.Font(family = "Century Gothic", size = 10)
 
@@ -153,29 +151,55 @@ class aplic():
         SH.set(0)
         SH.place(x = 250, y = 35)
 
-        SA = Scale(W, from_=0, to=150, tickinterval= 50, orient=HORIZONTAL, length=185, bg = '#F0B27A', bd = 0, highlightbackground = '#F0B27A', label = 'Potencial Aceleración', font = fontt)
+        SA = Scale(W, from_=0, to=2500, tickinterval= 500, orient=HORIZONTAL, length=250, bg = '#F0B27A', bd = 0, highlightbackground = '#F0B27A', label = 'Potencial Aceleración', font = fontt)
         SA.set(0)
-        SA.place(x = 10, y = 120)
-
-#Botones
+        SA.place(x = 10, y = 130)
 
 
+
+
+
+
+
+#Gráfica
+
+        def animate(i):
+            #self.graph.update()
+            line.set_ydata(np.sin(x+i/10.0))  # update the data
+            return line,
+
+        x = np.arange(0, 2*np.pi, 0.01)  
+        
+        CA = FigureCanvasTkAgg(fig, master=self.graph)
+        CA.get_tk_widget().place(x = 0, y = 0)
+
+        ax = fig.add_subplot(111)
+        line, = ax.plot(x, np.sin(x))
+
+        ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), interval=25, blit=False)
+        self.root.update()
 
         
+            
+
+
+
+
+
 
 #Bucle de las partículas
         while True:
             try:
-                self.root.update()
+                
                 Ml = Moleculas(self.Can, 350, 350, 10, 10, -149, "L")
                 mole.append(Ml)
-                Ms = Moleculas(self.Can, 350, 350, 10, 10, 251, "S")
+                Ms = Moleculas(self.Can, 350, 350, 10, 10, 280, "S")
                 mole.append(Ms)
 
                 while True:
                     try:
                         self.root.update()
-                        #print("esta")
+                        print("esta")
                         for m in mole:
                             if m.tipo == "S":
                                 m.update(0.0000001, SA.get(), SH.get())
@@ -186,6 +210,9 @@ class aplic():
                 mole.clear()
             except:
                 break
+
+
+        
 
 
 
